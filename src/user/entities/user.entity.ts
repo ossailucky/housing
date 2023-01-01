af3040f5b0c70126699f23b9539a5b4a39d6a380
Ossai Lucky <luckyblaqy@gmail.com>
2023-01-01 17:24:33 +0100

    added user schema

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose,{ Document} from "mongoose";
+
+export type UserDocument = User & Document
+
+@Schema({timestamps:true})
+export class User {
+  @Prop({ type: String })
+  name: string;
+  @Prop({ type: String, index: true })
+  email: string;
+  @Prop({ type: Number, required: true, unique: true })
+  phone: number;
+  @Prop({ type: String, required: true, index: true })
+  password: string;
+  @Prop({ type: String, index:true })
+  sex: string;
+  @Prop({ type: Boolean, default: false })
+  isAgent: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
