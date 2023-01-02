7687f9b50dab17b5b4e870d566d9d73c5604aa6f
Ossai Lucky <luckyblaqy@gmail.com>
2023-01-02 21:07:41 +0100

    add authentication endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
+import { AuthDTO } from './dto/auth.user';
 
-@Controller('auth')
+@Controller({path:'auth', version: "1"})
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  async login(@Res() res: Response, @Body() authDto: AuthDTO){
+    const { account, access_token } = await this.authService.validate(authDto);
+
+    res.setHeader('Authorization', `Bearer ${access_token}`);
+    res.json({ account, access_token });
+    return account
+  }
+    
 }
